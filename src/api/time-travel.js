c9e4f35ba9ea7bf0f391e9d42939bd552b80d5a1
Ola <ola@olavea.com>
2021-08-25 13:42:44 +0200

    fix: if (city && year) {res.json({ 👍 })}else{😬 }

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,4 +1,5 @@
 
+
 const PiRATE_PRiNCESS = {
     age: "11",
     hairColor: "gold",
@@ -8,6 +9,9 @@ export default async function handler(req, res) {
     const { city, year } = req.query;
     const message = `You time travelled to the city of ${city}, in year ${year} and teamed up with a Pirate `
 
-    res.json({ message: message, princess: PiRATE_PRiNCESS })
-    // res.send(message)
+    if (city === "oslo" && year === "2026" ) {
+        res.json({ message: message, princess: PiRATE_PRiNCESS })
+    } else {
+        res.send(message)
+    }
 }
