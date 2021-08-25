c8999665c6532887d5d279fbdff06fe7e044a5fc
Ola <ola@olavea.com>
2021-08-25 13:28:18 +0200

    fix: res.json({ message: 2026, princess: 👑  });

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -8,5 +8,6 @@ export default async function handler(req, res) {
     const { city, year } = req.query;
     const message = `You time travelled to the city of ${city}, in year ${year} and teamed up with a Pirate `
 
-    res.send(message)
+    res.json({ message: message, princess: PiRATE_PRiNCESS })
+    // res.send(message)
 }
