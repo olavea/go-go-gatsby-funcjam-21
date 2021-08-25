01b9b20ddcba7ca696f360cc0a4348168270a108
Ola <ola@olavea.com>
2021-08-25 13:21:02 +0200

    fix: add PiRATE_PRiNCESS object outside gatsbyFunc

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,3 +1,9 @@
+
+{
+    age: "11",
+    hairColor: "gold",
+}
+
 export default async function handler(req, res) {
     const { city, year } = req.query;
     res.send(`You time travelled to the city of ${city}, in year ${year}`)
