5a3d4239e12add1967d20b2e935e11b2103e0d6b
Ola <ola@olavea.com>
2021-08-25 13:17:21 +0200

    fix: get city in TimeShip browser into gatsby func

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,3 +1,4 @@
 export default async function handler(req, res) {
-    res.send("You time travelled to the city of Oslo in year 2026")
+    const { city } = req.query;
+    res.send(`You time travelled to the city of ${city}`)
 }
