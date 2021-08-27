f7559f969d35fc555af89e6794d347dc29ee698b
Ola <ola@olavea.com>
2021-08-27 09:04:04 +0200

    fix: copy text polish up 😺

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,23 +11,40 @@ const RubysTimeShipPage = () => {
         Built to save Ruby's sister, Princess Lizabeth from prison in the Tower of London.
       </p>
       <h2>Take a TimeShip Test Trip</h2>
-
-
       <ul>
         <li>
           <a href="/api/time-ship?city=oslo&year=2026&price=333">
             /api/time-ship?city=oslo&year=2026&price=333
           </a>
         </li>
+        <li>
+        <a href="/api/time-ship?city=London&year=1554&price=333">
+          /api/time-ship?city=London&year=1554&price=333
+        </a>
+      </li>
+      <li>
+      <a href="/api/time-ship?city=London&year=1554&price=333">
+        /api/time-ship?city= _ _ _ _ &year= _ _ _ _&price=333
+      </a>
+    </li>
       </ul>
-      <h2>Build Your Own Serverless Gatsby Function TimeShip.</h2>
-      <p>Clone the code of this TimeShip and get started:</p>
+      <h2>Build Your Own Gatsby Function TimeShip</h2>
+      <p>Clone the code of Ruby's TimeShip and get started:</p>
 
       <ul>
         <li>
           <a href="https://github.com/olavea/go-go-gatsby-funcjam-21">https://github.com/olavea/go-go-gatsby-funcjam-21</a>
         </li>
       </ul>
+      <h2>Or Bring Your Own Idea to a Gatsby Function</h2>
+      <p>Fabolous prizes await the winners, .... like a M1 Mac Miniiiiii.... Clone the oficcial Gatsby FuncJam repo and get started:</p>
+
+      <ul>
+        <li>
+          <a href="https://github.com/gatsby-inc/gatsby-funcjam-21">https://github.com/gatsby-inc/gatsby-funcjam-21</a>
+        </li>
+      </ul>
+
     </main>
   )
 }
